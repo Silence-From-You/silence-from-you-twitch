59a8a653fee4117c6393983264d19209a18b7f47
Jon <stricoff92@gmail.com>
2026-04-14 17:28:39 -0400

    wip

---
+++ readme.md
@@ -0,0 +1,14 @@
+### SHUTTUP TWITCH.TV!
+
+### SILENCE FROM YOU! YOURE CUTOFF!
+
+
+```bash
+# dev and test
+npm run dev:firefox
+
+# publish
+npm run zip:firefox
+```
+
+
--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,9 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     srcDir: 'src',
+    manifest: ({ mode }) => ({
+    content_security_policy: mode === 'development' ? {
+        extension_pages: "script-src 'self' http://localhost:43887; object-src 'self'"
+    } : undefined
+    })
 });
